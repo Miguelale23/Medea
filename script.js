7914db94c22ade462d81aef2b9819ecebb952c1e
miguelNPB <miguelnp@ucm.es>
2025-02-12 16:45:57 +0100

    fin

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-document.addEventListener("touchstart", function () {
+window.addEventListener("scroll", function () {
     let oculto1 = document.getElementById("oculto1");
-    let oculto2 = document.getElementById("oculto2");
 
     // Obtiene la posición de la sección con respecto al viewport
     let oculto1Position = oculto1.getBoundingClientRect().top;
-    let oculto2Position = oculto2.getBoundingClientRect().top;
 
     let windowHeight = window.innerHeight;
 
     // Si la sección está en la vista, activa la animación
-    if (oculto1Position < windowHeight * 1.5) { 
+    if (oculto1Position < windowHeight / 2) { 
         oculto1.classList.add("oculto1-shown");
-    }
-    if (oculto2Position < windowHeight * 1.5){
-        oculto2.classList.add("oculto2-shown");
     }
 });
